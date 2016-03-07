7ace1428ab3908e5f4c5125f2d0bcec9feabd3be
Victor Anjin <virinext@gmail.com>
2016-03-07 10:54:49 +0600

    testing samples relocated

--- hevcparser/tests/samples/get_samples.sh
+++ hevcparser/tests/samples/get_samples.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/49re63tsrgm3qzd/Sintel_272p_logo_30.265?dl=0 -O Sintel_272p_logo_30.265 -nc
-wget https://www.dropbox.com/s/vil4b0x7rxmv1f5/surfing_30.265?dl=0 -O surfing_30.265 -nc
-wget https://www.dropbox.com/s/wombxq3n7t55tt7/video-h265.hevc?dl=0 -O video-h265.hevc -nc
-wget https://www.dropbox.com/s/mr63k72js955384/10E_11345V_4T2_record_part.265?dl=0 -O 10E_11345V_4T2_record_part.265 -nc
-wget https://www.dropbox.com/s/r6uce1henzlbed3/TearsOfSteel_720p_h265_part.hevc?dl=0 -O TearsOfSteel_720p_h265_part.hevc -nc
-wget https://www.dropbox.com/s/k52mkv37ozri0z5/Jellyfish-3-Mbps-1080p-hevc_part.hevc?dl=0 -O Jellyfish-3-Mbps-1080p-hevc_part.hevc -nc
-wget https://www.dropbox.com/s/i28erkj91t0kra0/ffmpeg_default.hevc?dl=0 -O ffmpeg_default.hevc -nc
-wget https://www.dropbox.com/s/c4sfy9a6udoaxhp/ffmpeg_cp_tr_cm_2020.hevc?dl=0 -O ffmpeg_cp_tr_cm_2020.hevc -nc
-wget https://www.dropbox.com/s/w0vwk4itinuojma/f265_default.hevc?dl=0 -O f265_default.hevc -nc
-wget https://www.dropbox.com/s/pnkpuonmipbpnuu/homer_default.hevc?dl=0 -O homer_default.hevc -nc
-wget https://www.dropbox.com/s/0e8orlki0cp96s1/BQSquare_416x240_60_qp37.bin?dl=0 -O BQSquare_416x240_60_qp37.bin -nc
+wget https://www.dropbox.com/s/f7igqdmxi9ba8i7/Sintel_272p_logo_30.265?dl=0 -O Sintel_272p_logo_30.265 -nc
+wget https://www.dropbox.com/s/wjngaehonsgdb8u/surfing_30.265?dl=0 -O surfing_30.265 -nc
+wget https://www.dropbox.com/s/ntpugiwrpphbff7/video-h265.hevc?dl=0 -O video-h265.hevc -nc
+wget https://www.dropbox.com/s/zdwloz1v2b2eh2z/10E_11345V_4T2_record_part.265?dl=0 -O 10E_11345V_4T2_record_part.265 -nc
+wget https://www.dropbox.com/s/4hsd208hnm7o85f/TearsOfSteel_720p_h265_part.hevc?dl=0 -O TearsOfSteel_720p_h265_part.hevc -nc
+wget https://www.dropbox.com/s/3jhfgpegt09uy3m/Jellyfish-3-Mbps-1080p-hevc_part.hevc?dl=0 -O Jellyfish-3-Mbps-1080p-hevc_part.hevc -nc
+wget https://www.dropbox.com/s/c754v5vnxae3ftm/ffmpeg_default.hevc?dl=0 -O ffmpeg_default.hevc -nc
+wget https://www.dropbox.com/s/y64y7l63yl4m1aq/ffmpeg_cp_tr_cm_2020.hevc?dl=0 -O ffmpeg_cp_tr_cm_2020.hevc -nc
+wget https://www.dropbox.com/s/ghqcmvvv6n78hb4/f265_default.hevc?dl=0 -O f265_default.hevc -nc
+wget https://www.dropbox.com/s/xqrj1igvvopdonv/homer_default.hevc?dl=0 -O homer_default.hevc -nc
+wget https://www.dropbox.com/s/o2ushkw4z4sxlln/BQSquare_416x240_60_qp37.bin?dl=0 -O BQSquare_416x240_60_qp37.bin -nc
+wget https://www.dropbox.com/s/uoluk07t65k804m/TestCase1_LifeOfPie_ReEncoded1080pX265_HDR_part.hevc?dl=0 -O TestCase1_LifeOfPie_ReEncoded1080pX265_HDR_part.hevc -nc
