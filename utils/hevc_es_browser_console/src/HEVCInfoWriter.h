e931444b0e84f0cfda2fb0df9860f5baeeae5b93
Chema Gonzalez <chemag@gmail.com>
2019-03-15 18:12:48 -0700

    browser: make print functions virtual

--- utils/hevc_es_browser_console/src/HEVCInfoWriter.h
+++ utils/hevc_es_browser_console/src/HEVCInfoWriter.h
@@ -23,19 +23,19 @@ class HEVCInfoWriter: public HEVC::Parser::Consumer
       HEVC::Parser::Info                 m_info;
     };
 
-    void writeNALHeader(const NALUInfo& naluInfo, std::ostream &out);
-    void writeVPS(std::shared_ptr<HEVC::VPS> pVPS, std::ostream &out);
-    void writeSPS(std::shared_ptr<HEVC::SPS> pSPS, std::ostream &out);
-    void writePPS(std::shared_ptr<HEVC::PPS> pPPS, std::ostream &out);
-    void writeSlice(std::shared_ptr<HEVC::Slice> pSlice, std::ostream &out);
-    void writeAUD(std::shared_ptr<HEVC::AUD> pAUD, std::ostream &out);
-    void writeSEI(std::shared_ptr<HEVC::SEI> pSEI, std::ostream &out);
+    virtual void writeNALHeader(const NALUInfo& naluInfo, std::ostream &out);
+    virtual void writeVPS(std::shared_ptr<HEVC::VPS> pVPS, std::ostream &out);
+    virtual void writeSPS(std::shared_ptr<HEVC::SPS> pSPS, std::ostream &out);
+    virtual void writePPS(std::shared_ptr<HEVC::PPS> pPPS, std::ostream &out);
+    virtual void writeSlice(std::shared_ptr<HEVC::Slice> pSlice, std::ostream &out);
+    virtual void writeAUD(std::shared_ptr<HEVC::AUD> pAUD, std::ostream &out);
+    virtual void writeSEI(std::shared_ptr<HEVC::SEI> pSEI, std::ostream &out);
 
-    void writeProfileTierLevel(const HEVC::ProfileTierLevel &ptl, std::ostream &out, const std::string &prefix);
-    void writeHrdParameters(const HEVC::HrdParameters &hrd, uint8_t commonInfPresentFlag, std::ostream &out, const std::string &prefix);
-    void writeSubLayerHrdParameters(const HEVC::SubLayerHrdParameters &slhrd, uint8_t sub_pic_hrd_params_present_flag, std::ostream &out, const std::string &prefix);
-    void writeShortTermRefPicSet(std::size_t stRpsIdx, std::size_t num_short_term_ref_pic_sets, const std::vector<HEVC::ShortTermRefPicSet> &refPicSets, std::ostream &out, const std::string &prefix);
-    void writeVuiParameters(const HEVC::VuiParameters &vui, std::size_t sps_max_sub_layers_minus1, std::ostream &out, const std::string &prefix);
+    virtual void writeProfileTierLevel(const HEVC::ProfileTierLevel &ptl, std::ostream &out, const std::string &prefix);
+    virtual void writeHrdParameters(const HEVC::HrdParameters &hrd, uint8_t commonInfPresentFlag, std::ostream &out, const std::string &prefix);
+    virtual void writeSubLayerHrdParameters(const HEVC::SubLayerHrdParameters &slhrd, uint8_t sub_pic_hrd_params_present_flag, std::ostream &out, const std::string &prefix);
+    virtual void writeShortTermRefPicSet(std::size_t stRpsIdx, std::size_t num_short_term_ref_pic_sets, const std::vector<HEVC::ShortTermRefPicSet> &refPicSets, std::ostream &out, const std::string &prefix);
+    virtual void writeVuiParameters(const HEVC::VuiParameters &vui, std::size_t sps_max_sub_layers_minus1, std::ostream &out, const std::string &prefix);
 
     std::vector<NALUInfo>    m_nalus;
 
